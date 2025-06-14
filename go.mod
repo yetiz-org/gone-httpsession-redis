17e2a7979c78468d83c22c44c013e600aacc3353
Yeti Sno <yeti@yetiz.org>
2025-06-14 20:54:32 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,33 @@
+module github.com/yetiz-org/gone-httpsession-redis
+
+go 1.24
+
+require (
+	github.com/pkg/errors v0.8.1
+	github.com/yetiz-org/gone v1.6.2
+	github.com/yetiz-org/goth-datastore v1.1.1
+)
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.9.3 // indirect
+	github.com/gocql/gocql v1.7.0 // indirect
+	github.com/golang/snappy v1.0.0 // indirect
+	github.com/gomodule/redigo v1.9.2 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/hailocab/go-hostpool v0.0.0-20160125115350-e80d13ce29ed // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
+	github.com/yetiz-org/goth-base62 v0.0.0-20250611115419-c6835bcfab05 // indirect
+	github.com/yetiz-org/goth-bytebuf v0.0.0-20250611120548-fb5bbb6566e6 // indirect
+	github.com/yetiz-org/goth-kklogger v0.0.0-20250611122328-03d2bbd5bc69 // indirect
+	github.com/yetiz-org/goth-kkutil v0.0.0-20250611122830-a80c97dfb90a // indirect
+	github.com/yetiz-org/goth-secret v1.1.2 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/text v0.26.0 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gorm.io/driver/mysql v1.6.0 // indirect
+	gorm.io/gorm v1.30.0 // indirect
+)
---
+++ sessionprovider.go
@@ -0,0 +1,132 @@
+package redis
+
+import (
+	"encoding/json"
+	"github.com/pkg/errors"
+	"github.com/yetiz-org/gone/ghttp/httpsession"
+	datastore "github.com/yetiz-org/goth-datastore"
+	"time"
+)
+
+var (
+	SessionPrefix = "httpsession"
+)
+
+func sessionPrefix() string {
+	return SessionPrefix + "-gts" + ":"
+}
+
+func sessionKey(sessionId string) string {
+	return sessionPrefix() + sessionId
+}
+
+type SessionProvider struct {
+	Master *datastore.RedisOp
+	Slave  *datastore.RedisOp
+}
+
+func (s *SessionProvider) NewSession(expire time.Time) httpsession.Session {
+	session := httpsession.NewDefaultSession(s)
+	session.SetExpire(expire)
+	return session
+}
+
+func (s *SessionProvider) Sessions() map[string]httpsession.Session {
+	sessions := make(map[string]httpsession.Session)
+	op := s.Slave
+	if op == nil {
+		op = s.Master
+	}
+
+	if op == nil {
+		return sessions
+	}
+
+	keys := op.Keys(sessionPrefix() + "*")
+	for _, entity := range keys.GetSlice() {
+		if entity.GetString() != "" {
+			session := &httpsession.DefaultSession{}
+			if err := json.Unmarshal([]byte(entity.GetString()), session); err == nil {
+				sessions[session.Id()] = session
+			}
+		}
+	}
+
+	return sessions
+}
+
+func (s *SessionProvider) Session(key string) httpsession.Session {
+	op := s.Slave
+	if op == nil {
+		op = s.Master
+	}
+
+	if op == nil {
+		return nil
+	}
+
+	entity := op.Get(sessionKey(key))
+	if entity.Error == nil {
+		session := &httpsession.DefaultSession{}
+		if err := json.Unmarshal([]byte(entity.GetString()), session); err == nil {
+			return session
+		}
+	}
+
+	return nil
+}
+
+func (s *SessionProvider) Save(session httpsession.Session) error {
+	op := s.Master
+	if op == nil {
+		op = s.Slave
+	}
+
+	if op == nil {
+		return errors.Errorf("provider is nil")
+	}
+
+	if session == nil {
+		return errors.Errorf("session is nil")
+	}
+
+	ttl := int64(session.Expire().Sub(time.Now()).Seconds())
+	if ttl <= 0 {
+		ttl = 1
+	}
+
+	sessionData, _ := json.Marshal(session)
+	if err := op.SetExpire(sessionKey(session.Id()), string(sessionData), ttl).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *SessionProvider) Delete(key string) {
+	op := s.Master
+	if op == nil {
+		op = s.Slave
+	}
+
+	if op == nil {
+		return
+	}
+
+	op.Delete(sessionKey(key))
+}
+
+func NewSessionProvider(profileName string) *SessionProvider {
+	provider := &SessionProvider{}
+	redis := datastore.NewRedis(profileName)
+	provider.Master = redis.Master()
+	provider.Slave = redis.Slave()
+	return provider
+}
+
+func NewSessionProviderWithRedis(redis *datastore.Redis) *SessionProvider {
+	provider := &SessionProvider{}
+	provider.Master = redis.Master()
+	provider.Slave = redis.Slave()
+	return provider
+}
